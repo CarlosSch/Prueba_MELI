31bbebe19f8343b600c898708ef6f58ac2013453
Carlos Lopez <stractovariuz@gmail.com>
2024-02-22 19:47:09 -0600

    fix: add generic ElementType

--- front_MELI/src/app/components/MapRender.tsx
+++ front_MELI/src/app/components/MapRender.tsx
@@ -1,10 +1,10 @@
-import { Children, ReactNode } from "react";
+import { Children, ReactNode } from 'react';
 
 interface Props<ElementType> {
   of: ElementType[];
   render: (item: ElementType, index: number) => ReactNode;
 }
 
-export const MapRender = <A,>({ render, of }: Props<A>) => {
+export const MapRender = <ElementType,>({ render, of }: Props<ElementType>) => {
   return Children.toArray(of.map((item, index) => render(item, index)));
 };
