18a15632cb1b56c0aeb6922ea68b39e99b272fea
Carlos Lopez <stractovariuz@gmail.com>
2024-02-29 15:47:32 -0600

    refactor: add axios cache

--- front_MELI/src/app/api/fetchApi.ts
+++ front_MELI/src/app/api/fetchApi.ts
@@ -1,16 +1,14 @@
 import axios from "axios";
+import { setupCache } from "axios-cache-interceptor";
 
-const fetchApi = axios.create({
+const axiosInstance = axios.create({
   baseURL: "http://localhost:4000/api",
 });
 
-fetchApi.interceptors.request.use(
-  (config) => {
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  }
+axiosInstance.interceptors.request.use(
+  config => config,
+  error => Promise.reject(error),
 );
 
+const fetchApi = setupCache(axiosInstance);
 export default fetchApi;
